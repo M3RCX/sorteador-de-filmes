e0037260c41104cbb9d04c8361e9bd0de9ed3f27
Guilherme Fernandes <guilherme.fernandes@keyrus.com.br>
2023-01-16 11:03:38 -0300

    adição do provider

--- main.js
+++ main.js
@@ -14,34 +14,9 @@ $(".button-container").on("click", function () {
   $(".movie-title h2").remove();
   $(".movie-description p").remove();
   $(".movie-img img").remove();
-<<<<<<< HEAD
   $(".movie-provider p").remove();
   $(".movie-provider-img img").remove();
-=======
-  var i = 0;
-  var page = Math.floor(Math.random() * 501 + 1);
-  var categorieId = Math.floor(Math.random() * 20 + 1);
 
-  if ($(".onoffbtn").hasClass("active")) {
-    
-    var page = Math.floor(Math.random() * 17 + 1);
-
-    $.ajax({
-      type: "GET",
-      url: `https://api.themoviedb.org/3/discover/tv?api_key=ce20ac06fb3262b6ef00dd5c451648f1&language=pt-BR&sort_by=popularity.desc&page=${page}&include_null_first_air_dates=false&watch_region=BR&with_watch_monetization_types=flatrate&with_status=0&with_type=0`,
-      accept: "application/json",
-      contentType: "application/json",
-      dataType: "json",
-      success: function (response) {
-        console.log(categorieId);
-        console.log(response);
-        console.log(response.results);
->>>>>>> 642aa4ff8a50cb1f2da9ca55149473ee25133259
-
-        i = response.results[categorieId];
-        console.log(i);
-
-<<<<<<< HEAD
   var i = 0;
   var page = Math.floor(Math.random() * 501 + 1);
   // var categorieId = 1;
@@ -144,40 +119,6 @@ $(".button-container").on("click", function () {
           }
         });
 
-=======
-        var movieTitle = response.results[categorieId].name;
-        var movieDescription = response.results[categorieId].overview;
-        var movieImg = response.results[categorieId].poster_path;
-
-        $(".movie-img").append(`<img src='${IMG_URL}${movieImg}' />`);
-        $(".movie-title").append("<h2>" + movieTitle + "</h2>");
-        $(".movie-description").append("<p>" + movieDescription + "</p>");
-      },
-      error: function () {
-        $(".movie-title").append(
-          "<h2>Nenhum filme encontrado tente denovo</h2>"
-        );
-      },
-    });
-  } else {
-    $.ajax({
-      type: "GET",
-      url: `https://api.themoviedb.org/3/discover/movie?api_key=ce20ac06fb3262b6ef00dd5c451648f1&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&watch_region=BR&with_watch_monetization_types=flatrate`,
-      accept: "application/json",
-      contentType: "application/json",
-      dataType: "json",
-      success: function (response) {
-        console.log(categorieId);
-        console.log(response);
-        console.log(response.results);
-
-        i = response.results[categorieId];
-        console.log(i);
-
-        var movieTitle = response.results[categorieId].title;
-        var movieDescription = response.results[categorieId].overview;
-        var movieImg = response.results[categorieId].poster_path;
->>>>>>> 642aa4ff8a50cb1f2da9ca55149473ee25133259
 
         $(".movie-img").append(`<img src='${IMG_URL}${movieImg}' />`);
         $(".movie-title").append("<h2>" + movieTitle + "</h2>");
